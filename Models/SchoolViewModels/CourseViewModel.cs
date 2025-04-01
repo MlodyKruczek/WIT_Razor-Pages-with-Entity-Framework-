36f091d2beb4ed8c56a237f29724d80a57cde511
Michał Krukowski <michal.kru@wp.pl>
2025-04-01 15:40:01 +0200

    Part 6 - Dnoe

---
+++ Models/SchoolViewModels/CourseViewModel.cs
@@ -0,0 +1,9 @@
+public class CourseViewModel
+{
+    public int CourseID { get; set; }
+    public string Title { get; set; }
+    public int Credits { get; set; }
+    public string DepartmentName { get; set; }
+
+    public string InstructorName { get; set; }
+}
---
+++ Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,31 @@
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<CourseViewModel> CourseVM { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            CourseVM = await _context.Courses.Select(p => new CourseViewModel
+            {
+                CourseID = p.CourseID,
+                Title = p.Title,
+                Credits = p.Credits,
+                DepartmentName = p.Department.Name,
+            }).ToListAsync();
+        }
+    }
+}
