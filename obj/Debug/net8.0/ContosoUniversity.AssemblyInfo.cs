cd58abf36b6eed5514cdf28c8fb2113372139c99
Michał Krukowski <michal.kru@wp.pl>
2025-04-01 16:27:58 +0200

    Part 8 - Done

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f091d2beb4ed8c56a237f29724d80a57cde511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2de837daa2a3cf6590962eed1177ba0307b3f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
